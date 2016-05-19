25f231c7007aa05da7df7608559606ddb4453bef
jeffchang5 <chang.jeffrey25@gmail.com>
2016-05-19 14:37:12 -0400

    started the project

--- application/config/gplace_api.js
+++ application/config/gplace_api.js
@@ -1,4 +1,5 @@
 module.exports = {
-	key: 'AIzaSyBMF93-_T4glakYpbG4xESohfBq7pFkcVE',
-	url: ''
+	key: process.env.GOOGLE_API_KEY,
+	url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?',
+	parameters: 'query=restaurants&location=42.260189,-83.027957&radius=5000&name=tim%20hortons'
 }
--- application/server.js
+++ application/server.js
@@ -1,7 +1,35 @@
 'use strict';
 var express = require('express');
 var app = express();
-var gplace_api = require('./config/gplace_api_key');
+var port = process.env.PORT || 8080;
+var gplace_api = require('./config/gplace_api');
+var queryURL = (gplace_api.url +
+			gplace_api.parameters +
+			'&key=' +
+			gplace_api.key
+);
 
-app.get('')
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/bower_components'));
+app.get('/', function(req, res) {
+	// if (req.isAuthenticated = false) {
+	// 	res.render('intro');	
+	// }
+	//res.render('intro');
+	//res.sendFile('index.html', {root: __dirname} );
+
+});
+
+app.get('/api/create', function(req, res) {
+
+});
+
+app.post('/api/view/id', function(req, res, next) {
+	res.redirect('/users/' + req.username);
+
+});
+
+console.log('Listening on port ' + port);
+app.listen(port);
+
 
